358abb1e34ca3a316495c163c6900b267a313af9
pkpkp-pk <65728699+pkpkp-pk@users.noreply.github.com>
2021-07-04 09:37:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import output_format
 import anime_parser.anime
 import search
 
-TOKEN = '*TOKEN*' #Replace *TOKEN* to your telegram bot token. Example: 123456789:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw
+TOKEN = '1455138895:AAH8gK7va5O0tRPIVaJT3a5d5RCLbodbBlQ' #Replace *TOKEN* to your telegram bot token. Example: 123456789:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw
 
 bot = telebot.TeleBot(TOKEN)
 
